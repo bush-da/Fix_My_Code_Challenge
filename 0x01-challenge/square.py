d55200b253bcdb336e13deb17711bc4db4849f1c
Daniel Tujuma <danielsevani12@gmail.com>
2024-03-19 14:14:36 -0400

    fix minor issue

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,37 +1,37 @@
 #!/usr/bin/python3
-"""
-Module defining a square class
-"""
+'''
+The module for defining a square class
+'''
 
 
 class Square():
-    """
-    The Square class
-    """
+    '''
+    The square class
+    '''
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """initializing an object"""
+        '''For initializing an object of the class'''
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.height * self.width
 
-    def PermiterOfMySquare(self):
-        """ Calculate the permiter of square """
+    def perimeter_of_my_square(self):
+        '''Calculates and returns the perimeter of a square'''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ string representation """
+        '''String representation of the object'''
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    """ testing the class """
+    '''Testing the class'''
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
